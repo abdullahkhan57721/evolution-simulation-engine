01d071e6cb19bcec25d737c053b4f8fbefb7fb5b
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-08-24 15:04:39 -0400

    Export genetic phenotype growth rate

--- src/evo_engine/growth/__init__.py
+++ src/evo_engine/growth/__init__.py
@@ -1,8 +1,13 @@
 """Growth models for organism development."""
 
-from evo_engine.growth.models import FixedGrowthRate, GrowthModel
+from evo_engine.growth.models import (
+    FixedGrowthRate,
+    GeneticPhenotypeGrowthRate,
+    GrowthModel,
+)
 
 __all__ = [
     "FixedGrowthRate",
+    "GeneticPhenotypeGrowthRate",
     "GrowthModel",
 ]
