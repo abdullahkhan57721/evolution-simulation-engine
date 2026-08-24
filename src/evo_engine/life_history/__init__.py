d83971696d79ffa210e049fe15888d07ecc72994
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-08-24 11:33:52 -0400

    Export lifespan strategy models

--- src/evo_engine/life_history/__init__.py
+++ src/evo_engine/life_history/__init__.py
@@ -8,12 +8,26 @@ from evo_engine.life_history.energy_thresholds import (
     determine_energy_threshold,
     validate_energy_threshold_source,
 )
+from evo_engine.life_history.lifespan import (
+    DevelopmentalMaximumAge,
+    FixedMaximumAge,
+    MaximumAgeModel,
+    MaximumAgeSource,
+    determine_maximum_age,
+    validate_maximum_age_source,
+)
 
 __all__ = [
     "DevelopmentalEnergyThreshold",
+    "DevelopmentalMaximumAge",
     "EnergyThresholdModel",
     "EnergyThresholdSource",
     "FixedEnergyThreshold",
+    "FixedMaximumAge",
+    "MaximumAgeModel",
+    "MaximumAgeSource",
     "determine_energy_threshold",
+    "determine_maximum_age",
     "validate_energy_threshold_source",
+    "validate_maximum_age_source",
 ]
