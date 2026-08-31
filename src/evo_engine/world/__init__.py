86e6ba027ebd0064c8efe70506df46ada5a45c9b
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-08-31 13:26:42 -0400

    Export carcass lifecycle adapters

--- src/evo_engine/world/__init__.py
+++ src/evo_engine/world/__init__.py
@@ -1,18 +1,22 @@
 """World entities and mutable world state."""
 
-from evo_engine.world.access import WorldOrganismAccess
-from evo_engine.world.admission import WorldOrganismAdmission
+from evo_engine.world.access import WorldCarcassAccess, WorldOrganismAccess
+from evo_engine.world.admission import WorldCarcassAdmission, WorldOrganismAdmission
 from evo_engine.world.carcass import Carcass
-from evo_engine.world.departure import WorldOrganismDeparture
+from evo_engine.world.departure import WorldCarcassDeparture, WorldOrganismDeparture
 from evo_engine.world.environment import EnvironmentalField
 from evo_engine.world.organism import Organism
-from evo_engine.world.reference import WorldOrganismReference
+from evo_engine.world.reference import WorldCarcassReference, WorldOrganismReference
 from evo_engine.world.world_state import WorldState
 
 __all__ = [
     "Carcass",
     "EnvironmentalField",
     "Organism",
+    "WorldCarcassAccess",
+    "WorldCarcassAdmission",
+    "WorldCarcassDeparture",
+    "WorldCarcassReference",
     "WorldOrganismAccess",
     "WorldOrganismAdmission",
     "WorldOrganismDeparture",
