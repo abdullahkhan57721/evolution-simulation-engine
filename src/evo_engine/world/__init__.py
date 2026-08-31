cd679641243011234df85c95e7b1f1c1e097b3af
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-08-31 14:29:13 -0400

    Export world mutation records from world domain

--- src/evo_engine/world/__init__.py
+++ src/evo_engine/world/__init__.py
@@ -5,18 +5,36 @@ from evo_engine.world.admission import WorldCarcassAdmission, WorldOrganismAdmis
 from evo_engine.world.carcass import Carcass
 from evo_engine.world.departure import WorldCarcassDeparture, WorldOrganismDeparture
 from evo_engine.world.environment import EnvironmentalField
+from evo_engine.world.mutations import (
+    CarcassAdded,
+    CarcassRemoved,
+    EnvironmentalValueChanged,
+    OrganismAdded,
+    OrganismMoved,
+    OrganismRemoved,
+    ResourcesChanged,
+    WorldMutation,
+)
 from evo_engine.world.organism import Organism
 from evo_engine.world.reference import WorldCarcassReference, WorldOrganismReference
 from evo_engine.world.world_state import WorldState
 
 __all__ = [
     "Carcass",
+    "CarcassAdded",
+    "CarcassRemoved",
     "EnvironmentalField",
+    "EnvironmentalValueChanged",
     "Organism",
+    "OrganismAdded",
+    "OrganismMoved",
+    "OrganismRemoved",
+    "ResourcesChanged",
     "WorldCarcassAccess",
     "WorldCarcassAdmission",
     "WorldCarcassDeparture",
     "WorldCarcassReference",
+    "WorldMutation",
     "WorldOrganismAccess",
     "WorldOrganismAdmission",
     "WorldOrganismDeparture",
