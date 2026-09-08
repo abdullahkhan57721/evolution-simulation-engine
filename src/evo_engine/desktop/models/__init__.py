52a55e6bdb47ad639752c33a3a3612e0952e0e40
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-09-08 10:14:04 -0400

    Add desktop item-model package

---
+++ src/evo_engine/desktop/models/__init__.py
@@ -0,0 +1,5 @@
+"""Qt item models exposing presentation-only scalar values to QML."""
+
+from evo_engine.desktop.models.world import WorldOrganismModel, WorldResourceModel
+
+__all__ = ["WorldOrganismModel", "WorldResourceModel"]
