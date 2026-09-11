f7bf07c3de9ea40d73e84687a1fd0aeafcba6b00
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-09-11 14:58:31 -0400

    Q4: export native Presentation controller

--- src/evo_engine/desktop/controllers/__init__.py
+++ src/evo_engine/desktop/controllers/__init__.py
@@ -3,6 +3,7 @@
 from evo_engine.desktop.controllers.application import ApplicationController
 from evo_engine.desktop.controllers.evidence import EvidenceAuthoringController
 from evo_engine.desktop.controllers.experiment import ExperimentAuthoringController
+from evo_engine.desktop.controllers.presentation import PresentationController
 from evo_engine.desktop.controllers.reference import ReferenceStudyController
 from evo_engine.desktop.controllers.results import ResultsController
 from evo_engine.desktop.controllers.run import RunController
@@ -12,6 +13,7 @@ __all__ = [
     "ApplicationController",
     "EvidenceAuthoringController",
     "ExperimentAuthoringController",
+    "PresentationController",
     "ReferenceStudyController",
     "ResultsController",
     "RunController",
